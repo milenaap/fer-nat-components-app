0f062116db53a3a9b937568e7fbfa99931ecc57a
Milena <darimile@gmail.com>
2025-09-03 20:29:25 +0200

    add Inifinity scroll

---
+++ presentation/images/FadeInImage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+
+
+const FadeInImage = () => {
+  return (
+    <View>
+      <Text>FadeInImage</Text>
+    </View>
+  )
+}
+
+export default FadeInImage
